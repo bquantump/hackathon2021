c4e9d91925b8d68ba542c06b2afae0b71a38713f
bquantump <stevens@microsoft.com>
2021-10-12 17:22:33 -0700

    add map

--- eventhubscore/eventhubcore/launch.py
+++ eventhubscore/eventhubcore/launch.py
@@ -5,4 +5,4 @@ from eventhubcore import run_server
 if __name__ == '__main__':
     # start 4 worker processes
     with Pool(processes=4) as pool:
-        pool(run_server, [1,2,3,4])
+        pool.map(run_server, [1,2,3,4])
