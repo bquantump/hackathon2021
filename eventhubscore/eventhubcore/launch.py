31c20b619cf4305023125bf35e9275c67d1c31af
bquantump <stevens@microsoft.com>
2021-10-12 17:41:32 -0700

    fix type

--- eventhubscore/eventhubcore/launch.py
+++ eventhubscore/eventhubcore/launch.py
@@ -5,4 +5,4 @@ from eventhubcore import run_server
 if __name__ == '__main__':
     # start 4 worker processes
     with Pool(processes=4) as pool:
-        pool.map(run_server, ['$Default','$Default','$test1','test1'])
+        pool.map(run_server, ['$Default','$Default','test1','test1'])
